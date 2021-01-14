4852080d507a1db1f57f0fb90c3ce1868ae9c327
dbrignoli <davide.brignoli@yourbiz.it>
2021-01-14 16:46:51 +0100

    complete slider.

--- assets/js/scriptMain.js
+++ assets/js/scriptMain.js
@@ -4,14 +4,17 @@ $(document).ready(function () {
         slidesToScroll: 1,
 
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 5000,
 
         speed: 500,
         fade: true,
         cssEase: 'linear',
 
-        
+        dots: true,
+        infinite: true,
+
         prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><img src="assets/img/prev-arrow.png"></button>',
-        nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button" style=""><img src="assets/img/next-arrow.png"></button>'
-  });
+        nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button" style=""><img src="assets/img/next-arrow.png"></button>',
+        
+    });
 });
